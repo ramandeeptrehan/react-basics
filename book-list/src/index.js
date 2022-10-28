be0bdf7a906b10e8029bde5032dafd538559b2c7
rtrehan <rtrehan@rtrehans-MacBook-Pro.local>
2022-10-27 22:23:40 -0700

    Different components for book list

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+  //could have multiple components of same type
+  /*
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+  */
+  return (
+    <section>
+      <Book />
+      <Image />
+      <Title />
+      <Author />
+    </section>
+  );
+}
+
+const Book = () => {
+  return <article>This is a book</article>;
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/41WTatssdDL._SX311_BO1,204,203,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>The War Of Art</h1>;
+};
+
+const Author = () => {
+  return <h4>Steven Pressfield</h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
