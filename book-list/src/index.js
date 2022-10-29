40b0cf1a01caa71dfd7cf774084014c2454531a3
rtrehan <rtrehan@rtrehans-MacBook-Pro.local>
2022-10-29 00:49:59 -0700

    Remove React components and combine simple html tags in one component

--- book-list/src/index.js
+++ book-list/src/index.js
@@ -1,34 +1,13 @@
+//this code is when you don't write many components. You club simple things in one component
+//NOTE: change this filename to index.js and npm start will run this file
 import React from "react";
 import ReactDom from "react-dom";
 
 import "./index.css";
 
 function BookList() {
-  //could have multiple components of same type
-  /*
-  return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-  */
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
     </section>
   );
@@ -38,42 +17,13 @@ const Book = () => {
   return (
     <article className="book">
       <h2>This is a book</h2>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/41WTatssdDL._SX311_BO1,204,203,200_.jpg"
+        alt=""
+      />
+      <h1>The War Of Art</h1>
+      <h4>Steven Pressfield</h4>
     </article>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/41WTatssdDL._SX311_BO1,204,203,200_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>The War Of Art</h1>;
-};
-
-const Author = () => {
-  //return <h4>Steven Pressfield</h4>;
-  /*
-    JSX with CSS
-
-    style={} means JS code is being placed.
-    Another {} insider {} means object in JS code
-    You get this: {{}} //outer {} suggests it's JS code, internal {} suggests it's JS object used in JS code
-    Within that, values are key value, where values are Strings
-
-    Also, JSX CSS rules takes precedence over index.css file styling for specific component.
-  */
-  return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", margiTop: "0.25rem" }}>
-      Steven Pressfield
-    </h4>
   );
 };
 
